aa9fd323f6f6b24b2b96b45158fbbf9b74bad71f
Hassan Bulega <hassanbulega@gmail.com>
2022-06-19 10:03:42 +0300

    refactor: change the title for data

--- src/data.js
+++ src/data.js
@@ -1,31 +1,31 @@
 const questions = [
   {
     id: 1,
-    title: 'Do I have to allow the use of cookies?',
+    title: 'What is a Framework?',
     info:
       'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
   },
   {
     id: 2,
-    title: 'How do I change my My Page password?',
+    title: 'What Should I Learn Before a JS Framework?',
     info:
       'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
   },
   {
     id: 3,
-    title: 'What is BankID?',
+    title: 'How to Choose From a Plethora of Frameworks?',
     info:
       'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
   },
   {
     id: 4,
-    title: 'Whose birth number can I use?',
+    title: 'Why React or Vue or Angular?',
     info:
       'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
   },
   {
     id: 5,
-    title: 'When do I recieve a password ordered by letter?',
+    title: 'Why Professionals Start with React and Svelte?',
     info:
       'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
   },
